65ab6226f123b0cb90a323ba6558b23abf09ee2a
Suvashan Baniya <suvashanb@gmail.com>
2026-05-10 19:57:12 +0545

    Learning the git embeding

---
+++ embedd.py
@@ -0,0 +1,21 @@
+# learning the embedding of the text data  march 10/ 2026
+
+import requests
+import json
+url = "http://localhost:11434/api/embeddings"
+text = "happy person"
+
+respones = requests.post(
+    url,
+    json ={
+        "model":"nomic-embed-text",
+        "prompt":text
+    }
+)
+
+data = respones.json()
+embedding = data["embedding"]
+
+print("Frist 10 embedding numbers:\n")
+
+print(embedding[:10])
